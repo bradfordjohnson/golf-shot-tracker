447684c0d34e27df29df7b9281e84ff6ceeaec08
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-07-06 17:23:05 -0400

    update for club mvp

--- fastapi/main.py
+++ fastapi/main.py
@@ -26,16 +26,6 @@ class ClubsBase(BaseModel):
     brand: str
     model: str
     club_type: str
-    # loft: Optional[str] = None
-    # shaft_material: Optional[str] = None
-    # shaft_flex: Optional[str] = None
-    # length_inches: Optional[int] = None
-    # grip_type: Optional[str] = None
-    
-class ShotsBase(BaseModel):
-    club_id: int
-    timestamp: datetime
-    distance: int
     
 def get_db():
     db = SessionLocal()
@@ -51,42 +41,14 @@ async def get_clubs(db: db_dependency, skip: int=0, limit: int=100):
     clubs=db.query(models.Clubs).offset(skip).limit(limit).all()
     return clubs
 
-@app.get("/clubs/{club_id}")
-async def get_question(club_id: int, db: db_dependency):
-    result = db.query(models.Clubs).filter(models.Clubs.id == club_id).first()
-    if not result:
-        raise HTTPException(status_code=404, detail="Question not found")
-    return result
-
-
-@app.get("/shots/")
-async def get_shots(db: db_dependency, skip: int=0, limit: int=100):
-    shots=db.query(models.Shots).offset(skip).limit(limit).all()
-    return shots
-
 @app.post("/add/club/")
 async def add_club(club: ClubsBase, db: db_dependency):
     db_club = models.Clubs(
         brand = club.brand,
         model = club.model,
         club_type = club.club_type
-        # loft = club.loft,
-        # shaft_material = club.shaft_material,
-        # shaft_flex = club.shaft_flex,
-        # length_inches = club.length_inches,
-        # grip_type = club.grip_type
     )
     db.add(db_club)
     db.commit()
     db.refresh(db_club)
-
-@app.post("/add/shot/")
-async def add_shot(shot: ShotsBase, db: db_dependency):
-    db_shots = models.Clubs(
-        club_id = shot.club_id,
-        timestamp = shot.timestamp,
-        distance = shot.distance
-    )
-    db.add(db_shots)
-    db.commit()
-    db.refresh(db_shots)
+    return db_club
--- fastapi/models.py
+++ fastapi/models.py
@@ -1,12 +1,5 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, DateTime, Null
-
-# class Users(Base):
-#     __tablename__ = 'users'
-    
-#     id = Column(Integer, primary_key=True, index=True)
-#     first_name = Column(String)
-#     last_name = Column(String)
     
 class Clubs(Base):
     __tablename__ = 'clubs'
@@ -15,21 +8,3 @@ class Clubs(Base):
     brand = Column(String)
     model = Column(String)
     club_type = Column(String)
-    # loft = Column(String)
-    # shaft_material = Column(String)
-    # shaft_flex = Column(String)
-    # length_inches = Column(Integer)
-    # grip_type = Column(String)
-    
-class Shots(Base):
-    __tablename__ = 'shots'
-    
-    id = Column(Integer, primary_key=True, index=True)
-    club_id =  Column(Integer, ForeignKey("clubs.id"))
-    timestamp = Column(DateTime)
-    # location
-    # measurement_method
-    # unit
-    distance = Column(Integer)
-    # shot_type = Column(String)
-    # notes = Column(String)
