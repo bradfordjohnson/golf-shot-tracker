6a03966c088ea7d9501c04e9b3dd390630d9138e
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-07-07 11:58:42 -0400

    updated formatting style

--- fastapi/main.py
+++ fastapi/main.py
@@ -22,36 +22,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class ClubsBase(BaseModel):
     brand: str
     model: str
     club_type: str
-    
+
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
+
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @app.get("/clubs/", response_model=List[ClubsBase])
-async def get_clubs(db: db_dependency, skip: int=0, limit: int=100):
-    clubs=db.query(models.Clubs).offset(skip).limit(limit).all()
+async def get_clubs(db: db_dependency, skip: int = 0, limit: int = 100):
+    clubs = db.query(models.Clubs).offset(skip).limit(limit).all()
     return clubs
+
 
 @app.post("/add/club/")
 async def add_club(club: ClubsBase, db: db_dependency):
-    db_club = models.Clubs(
-        brand = club.brand,
-        model = club.model,
-        club_type = club.club_type
-    )
+    db_club = models.Clubs(brand=club.brand, model=club.model, club_type=club.club_type)
     db.add(db_club)
     db.commit()
     db.refresh(db_club)
     return {"message": "Club added successfully"}
+
 
 @app.delete("/delete/club/")
 async def delete_club(club_id: int, db: db_dependency):
--- fastapi/models.py
+++ fastapi/models.py
@@ -1,9 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, DateTime, Null
-    
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    Float,
+    ForeignKey,
+    DateTime,
+    Null,
+)
+
+
 class Clubs(Base):
-    __tablename__ = 'clubs'
-    
+    __tablename__ = "clubs"
+
     id = Column(Integer, primary_key=True, index=True)
     brand = Column(String)
     model = Column(String)
